635013f77f9d5d5a7178b8d0f872ddecb2285c7e
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-09-09 00:06:53 +0300

    added the Core-Chaos module.

---
+++ core-chaos/src/main/java/core/CoreChaos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package core;
+
+/**
+ * <b>Test class for this jar is not executable.</b>
+ * @author rash4
+ */
+public class CoreChaos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
