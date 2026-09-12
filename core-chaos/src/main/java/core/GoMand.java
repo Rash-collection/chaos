3a4e955761f89acd4f0d34f61066c81b9930d438
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-09-12 03:34:09 +0300

    the class to replace 'Gor' in CoreChaos, for now it's a place holder that extends bash.comds.Commanding for more tests and so on.

---
+++ core-chaos/src/main/java/core/GoMand.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package core;
+
+import bash.comds.Commanding;
+import bash.consoles.Console;
+
+/**
+ *
+ * @author rash4
+ */
+public class GoMand extends Commanding{
+    public GoMand(Console cons){
+        super(cons);
+    }
+
+}
