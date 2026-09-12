43dcdc691d25ef39d59fbba477ecde420ed235ec
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-09-12 08:00:23 +0300

    cleared for now, maybe removed later, because this is not executable jar

--- core-chaos/src/main/java/core/CoreChaos.java
+++ core-chaos/src/main/java/core/CoreChaos.java
@@ -4,24 +4,13 @@
 
 package core;
 
-import bash.comds.Command;
-import bash.comds.CommandTree;
-import bash.comds.Commanding;
-import bash.consoles.Console;
-
 /**
- * <b>Test class for this jar is not executable.</b>
+ * 
  * @author rash4
  */
 public class CoreChaos {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        final var conso = new bash.consoles.Console();
-        conso.initConsole();
-        final GoMand cmd = new GoMand(conso);
-        conso.setCommando(cmd);
-        cmd.addCommand(new CommandTree().setDirectCom(new Command(help->{return cmd.getHelp("");})), "help");
-        
     }
 }
